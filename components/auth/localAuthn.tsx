674abf12c99683ccdfd43371166eecf35d8701cf
GeorgeContreras241 <maicolcontreras241@gmail.com>
2026-06-16 16:37:15 +0200

    refactor(auth): Remove unused auth stubs

--- components/auth/localAuthn.tsx
+++
@@ -1,9 +0,0 @@
-// this component is for local authentication with master key and archive
-
-export const LocalAuthn = () => {
-    return (
-        <div>
-            <h1>Local Authentication</h1>
-        </div>
-    )
-}
