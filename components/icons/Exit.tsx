ab66fa06e93bf0f4520967b7636b3a90fefb716d
GeorgeContreras241 <maicolcontreras241@gmail.com>
2026-09-16 19:50:38 -0500

    chore: Remove unused commitGenerator and fix Exit icon

--- components/icons/Exit.tsx
+++ components/icons/Exit.tsx
@@ -1,5 +1,5 @@
 export const Exit = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="4" height="4" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M15 4.001H5v14a2 2 0 0 0 2 2h8m1-5l3-3m0 0l-3-3m3 3H9" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 4.001H5v14a2 2 0 0 0 2 2h8m1-5l3-3m0 0l-3-3m3 3H9" />
     )
 }
